bab1f513aa61c4ed728d8317b7dd25318492d55d
crispysodium <christinena.316@gmail.com>
2020-03-21 14:13:01 -0700

    updated schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-drop database if exists pokequizDB;
-
-create database pokequizDB;
-
-use pokequizDB;
-
-=======
 DROP DATABASE IF EXISTS pokequizDB;
 
 CREATE DATABASE pokequizDB;
->>>>>>> quiz-feature
