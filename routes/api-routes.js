ef72c41ac806a7cc6fe56de24c44aeaecc4f338f
setocourtney <courtneyjseto@gmail.com>
2020-03-24 14:45:03 -0700

    readded html route

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -51,4 +51,43 @@ module.exports = function(app) {
     }
   });
 
+  // -- Game Features --
+  app.get("/api/questions/:type_id", function(req, res) {
+    db.Questions.findAll({
+      where: {
+        type_id: req.params.type_id
+      }
+    }).then((dbQuestions) => {
+      res.json(dbQuestions);
+    });
+  });
+
+  app.get("/api/questions/", function(req, res) {
+    db.Questions.findAll({}).then((dbQuestions) => {
+      res.json(dbQuestions);
+    });
+  });
+
+  app.get("/api/pokedex/", function(req, res) {
+    db.Pokedex.findAll({}).then((dbPokedex) => {
+      res.json(dbPokedex);
+    });
+  });
+
+  app.post("/api/pokedex/", function(req, res) {
+    db.Pokedex.create({
+      pokemon_id: req.body.pokemon_id,
+      isCaptured: req.body.isCaptured,
+      user_id: req.body.user_id
+    }).then((dbPokedex) => {
+      res.json(dbPokedex);
+    });
+  });
+
+  app.get("/api/pokemon_data", function(req, res) {
+    db.Pokemon.findAll().then((pokemon) => {
+      res.json(pokemon);
+    });
+  });
+
 };
