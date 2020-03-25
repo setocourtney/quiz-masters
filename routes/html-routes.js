5e0549b9d46a65ee7d8266223d8b3cf70818abea
crispysodium <christinena.316@gmail.com>
2020-03-24 20:40:04 -0700

    added query for user's pokedex

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,11 +1,20 @@
+var db = require("../models");
+
 module.exports = function(app) {
 
   app.get("/", function(req, res) {
     res.render("start");
   });
 
-  app.get("/pokedex", function(req, res) {
-    res.render("pokedex");
+  app.get("/pokedex/:user_id", function(req, res) {
+    // week 13 act 9 
+    db.Pokedex.findAll({
+      where: {
+        user_id: req.params.user_id
+      }
+    }).then((dbPokedex) => {
+      res.render("pokedex");
+    });
   });
 
   app.get("/play", function(req, res) {
