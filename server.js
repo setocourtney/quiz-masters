65448f83a173e4abdf258a139cfdfc23ecdc95d4
crispysodium <christinena.316@gmail.com>
2020-03-24 22:10:04 -0700

    removed reference to html-routes.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.set("view engine", "handlebars");
 
 
 // Requiring our routes
-require("./routes/html-routes.js")(app);
+require("./routes/question-routes.js")(app);
 require("./routes/api-routes.js")(app);
 
 // Syncing our database and logging a message to the user upon success
