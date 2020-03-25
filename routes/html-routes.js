d8affc8228f57c75c47d086bd2de1f983333e4f6
setocourtney <courtneyjseto@gmail.com>
2020-03-24 19:35:16 -0700

    added html routes for rendering handlebars

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -7,5 +7,9 @@ module.exports = function(app) {
   app.get("/pokedex", function(req, res) {
     res.render("pokedex");
   });
-  
+
+  app.get("/play", function(req, res) {
+    res.render("index");
+  });
+
 };
