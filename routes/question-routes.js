e23861254498d2ed100c2fecfade75df5994bb3b
Ryan Tam <ryanjtam@gmail.com>
2020-03-24 19:42:40 -0700

    handlebar updates, added grass scene with model

--- routes/question-routes.js
+++ routes/question-routes.js
@@ -2,6 +2,37 @@ var db = require("../models");
 
 module.exports = function(app) {
 
+  app.get('/play', function(req, res) {
+    // db.Questions.findAll().then((questions) => {
+    //   res.render('index', { questions: questions});
+    //   res.json(questions);
+    // });
+    db.Pokemon.findAll().then((pokemon) => {
+      // console.log({pokemon: pokemon})
+      console.log(pokemon.length)
+
+      res.render('index', { pokemon: pokemon });
+    });
+  });
+
+  app.get('/play/:id', function(req, res) {
+
+    db.Pokemon.findOne({
+      where: {
+        pokeId: req.params.id
+      },
+      include: [db.Types]
+    }).then((pokemon) => {
+      db.Questions.findAll({
+        where: {
+          type_id: pokemon.type
+        }
+      }).then(questions)
+      res.render("battle", { pokemon: pokemon[0] })
+
+    });
+  })
+
   app.get("/api/questions/:type_id", function(req, res) {
     db.Questions.findAll({
       where: {
@@ -20,6 +51,16 @@ module.exports = function(app) {
 
   app.get("/api/pokemon_data", function(req, res) {
     db.Pokemon.findAll().then((pokemon) => {
+      res.json(pokemon);
+    });
+  });
+
+  app.get("/api/pokemon_data/:id", function(req, res) {
+    db.Pokemon.findAll({
+      where: {
+        pokeId: req.params.id
+      }
+    }).then((pokemon) => {
       res.json(pokemon);
     });
   });
--- server.js
+++ server.js
@@ -3,23 +3,32 @@ var express = require("express");
 // var session = require("express-session");
 // Requiring passport as we've configured it
 // var passport = require("./config/passport");
-var session = require("express-session");
+// var session = require("express-session");
 
 // Setting up port and requiring models for syncing
 var PORT = process.env.PORT || 8080;
 
+var app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+app.use(express.static(__dirname + '/public'));
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 var db = require("./models");
 
 // Creating express app and configuring middleware needed for authentication
-var app = express();
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(express.static("public"));
 // We need to use sessions to keep track of our user's login status
 // app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
 // app.use(passport.initialize());
 // app.use(passport.session());s
+
+
 
 // Requiring our routes
 require("./routes/question-routes.js")(app);
