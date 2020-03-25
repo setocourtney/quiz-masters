08274070f997aabaeeb14a7055c0bb77ae56f4ed
setocourtney <courtneyjseto@gmail.com>
2020-03-24 19:54:04 -0700

    fix server.js

--- server.js
+++ server.js
@@ -1,12 +1,5 @@
 // Requiring necessary npm packages
 var express = require("express");
-// var session = require("express-session");
-// Requiring passport as we've configured it
-// var passport = require("./config/passport");
-// var session = require("express-session");
-
-// Setting up port and requiring models for syncing
-var PORT = process.env.PORT || 8080;
 
 var session = require("express-session");
 
@@ -26,16 +19,6 @@ var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
-
-var db = require("./models");
-
-// Creating express app and configuring middleware needed for authentication
-// We need to use sessions to keep track of our user's login status
-// app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
-// app.use(passport.initialize());
-// app.use(passport.session());s
-
-
 
 
 // Requiring our routes
