88cf928132e13f559ffe5207c89948549bf5439c
setocourtney <courtneyjseto@gmail.com>
2020-03-24 19:22:07 -0700

    removed passport authentication files

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,31 +1,11 @@
-// Requiring path to so we can use relative routes to our HTML files
-var path = require("path");
-
-// Requiring our custom middleware for checking if a user is logged in
-var isAuthenticated = require("../config/middleware/isAuthenticated");
-
 module.exports = function(app) {
 
   app.get("/", function(req, res) {
-    // If the user already has an account send them to the members page
-    if (req.user) {
-      res.redirect("/members");
-    }
-    res.sendFile(path.join(__dirname, "../public/signup.html"));
+    res.render("start");
   });
 
-  app.get("/login", function(req, res) {
-    // If the user already has an account send them to the members page
-    if (req.user) {
-      res.redirect("/members");
-    }
-    res.sendFile(path.join(__dirname, "../public/login.html"));
+  app.get("/pokedex", function(req, res) {
+    res.render("pokedex");
   });
-
-  // Here we've add our isAuthenticated middleware to this route.
-  // If a user who is not logged in tries to access this route they will be redirected to the signup page
-  app.get("/members", isAuthenticated, function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/members.html"));
-  });
-
+  
 };
